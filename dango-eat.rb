b3ea1c19fb57d26a3cc1d247cfcf69bd3ff72ca0
zunda <zundan@gmail.com>
2026-04-29 12:59:55 +0900

    Show dango and stick

--- dango-eat.rb
+++ dango-eat.rb
@@ -5,29 +5,38 @@ led = WS2812.new(pin: Board43::GPIO_LEDOUT, num: 256)
 w = 16
 frame_interval = 100 # ms
 
-def rgba(r, g, b, a)
-  return [r*a, g*a, b*a].map{|x| x.round}
-end
-
 def dango_alpha(x, y, cx, cy, r)
   d = Math::sqrt((x - cx)**2 + (y - cy)**2)
   if d < r
-    return 1.0
+    return 1
   else
-    return 0.0
+    return 0
   end
 end
 
-loop do
-  w.times do |y|
-    w.times do |x|
-      a = dango_alpha(x, y, 7.5, 7.5, 2.1)
-      p a
-      rgb = rgba(0x80, 0xB0, 0x20, a)
-      led.set_rgb(y*w + x, *rgb)
+def stick_alpha(x, y, x1, y1, x2, y2, w)
+  d = ((y2 - y1)*x - (x2 - x1)*y + x2*y1 - y2*x1)/Math::sqrt((y2-y1)**2 + (x2-x1)**2)
+  d = -d if d < 0
+  if d*2 < w
+    return 1
+  else
+    return 0
+  end
+end
+
+d_rgb = [0x80, 0xB0, 0x20]
+s_rgb = [0x30, 0x10, 0x10]
+
+w.times do |y|
+  w.times do |x|
+    rgb = [0, 0, 0]
+    if dango_alpha(x, y, 7.5, 7.5, 3) > 0
+      rgb = d_rgb
+    elsif stick_alpha(x, y, 3, 3, w-4, w-4, 1.5) > 0
+      rgb = s_rgb
     end
+    led.set_rgb(y*w + x, *rgb)
   end
-  led.show
-
-  sleep_ms frame_interval
 end
+led.show
+
